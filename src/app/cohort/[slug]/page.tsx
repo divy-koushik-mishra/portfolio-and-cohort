d35edab92708077c591200ef7bae4432662a9829
Divy Koushik Mishra <divykoushikmishra@gmail.com>
2024-03-15 12:40:07 +0530

    Refactor cohort page and add Card component

--- src/app/cohort/[slug]/page.tsx
+++ src/app/cohort/[slug]/page.tsx
@@ -10,29 +10,23 @@ import {
 } from "@/components/ui/breadcrumb";
 import Faq from "@/components/CohortComponents/Faq";
 import { useState, useEffect } from "react";
-import Python from "@/components/CohortComponents/CourseInfo/Python";
-import Java from "@/components/CohortComponents/CourseInfo/Java";
-import C from "@/components/CohortComponents/CourseInfo/C";
+import Card from "@/components/CohortComponents/Card";
 
 export default function Page({ params }: { params: { slug: string } }) {
   const { slug } = params;
   const [thumbnailUrl, setThumbnailUrl] = useState("");
-  const [python, setPython] = useState(false);
-  const [java, setJava] = useState(false);
-  const [c, setC] = useState(false);
+  const cohortCurriculam = [{}, {}];
+
   useEffect(() => {
     if (slug === "python") {
       setThumbnailUrl("/tech/python.jpg");
-      setPython(true);
     } else if (slug === "java") {
       setThumbnailUrl("/tech/java.png");
-      setJava(true);
     } else if (slug === "c") {
       setThumbnailUrl("/tech/c.png");
-      setC(true);
     }
   }, [slug]);
-  // My Post: {params.slug}
+
   return (
     <section className="min-h-screen flex flex-col items-center w-full py-5 md:p-10">
       <div className="container h-4/5 flex flex-col md:flex-row items-center border-b border-gray-700 mb-20">
@@ -42,7 +36,7 @@ export default function Page({ params }: { params: { slug: string } }) {
             alt="hero"
             width={1920}
             height={1080}
-            className="w-full rounded-lg"
+            className="w-full rounded-lg  my-10"
           />
         </div>
         <div className="w-full md:w-3/5 py-4 md:p-10 ">
@@ -69,9 +63,10 @@ export default function Page({ params }: { params: { slug: string } }) {
           <div className="">
             <h3 className="text-xl font-semibold my-5">Curriculam</h3>
             <div className="">
-              {python ? <Python /> : null}
-              {java ? <Java /> : null}
-              {c ? <C /> : null}
+              <div className="flex flex-col md:flex-row gap-5 w-full">
+                <Card />
+                <Card />
+              </div>
 
               <button className="border p-3 rounded-[8px] my-5 hover:bg-[#ddd] hover:text-black transition-all">
                 Join Now
---
+++ src/components/CohortComponents/Card.tsx
@@ -0,0 +1,21 @@
+const Card = () => {
+  return (
+    <div className="border w-full md:w-1/2 p-5  rounded-xl">
+      <h2 className="text-xl font-bold">About the course</h2>
+      <p className="text-sm p-1 mb-2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis
+        eaque voluptatem illum vitae vel rerum asperiores voluptas? A rerum
+        necessitatibus doloribus dolores ex quam fugiat molestias veniam
+        aperiam, maxime, excepturi sunt at nemo asperiores? Nihil facilis quae
+        recusandae! Non voluptatem officia perferendis. Sunt quae adipisci
+        commodi omnis enim, laborum exercitationem.
+      </p>
+      <hr />
+      <button className="border hover:bg-[#ddd] hover:text-black border-white rounded-xl px-12 py-2 mt-5 transition">
+        Project
+      </button>
+    </div>
+  );
+};
+
+export default Card;
